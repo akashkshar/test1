8be924d4b7fce5634f94c0d690878b0f9919d5c1
root <root@localhost.localdomain>
2021-09-27 13:20:58 +0530

    First commit

---
+++ Test.java
@@ -0,0 +1,7 @@
+class Test {
+         public static void main(String[] args) {
+         int a=24,b=22;
+         int c=a+b;
+         System.out.println("The addition of a and b is " + c);
+             }
+}
